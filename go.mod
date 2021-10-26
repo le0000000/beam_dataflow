fda0dc5d3486fd70b1d26ce058b1a26c9ed2dfb6
Tao Liao <taoliao@chromium.org>
2021-10-26 10:06:58 -0400

    addfiles

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/google/privacy-sandbox-aggregation-service
+
+go 1.16
+
+require (
+	cloud.google.com/go v0.84.0
+	cloud.google.com/go/storage v1.15.0
+	github.com/apache/beam v2.32.0-RC1+incompatible
+	github.com/golang/lint v0.0.0-20180702182130-06c8688daad7 // indirect
+	github.com/google/glog v0.5.0 // indirect
+	github.com/google/tink/go v1.6.0 // indirect
+	github.com/googleapis/gax-go v2.0.2+incompatible
+	github.com/googleapis/gax-go/v2 v2.0.5
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	gonum.org/v1/gonum v0.8.2
+	google.golang.org/api v0.42.0
+	google.golang.org/protobuf v1.27.0
+)
---
+++ pipeline/simplesum_main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"reflect"
+	"strconv"
+
+	"github.com/apache/beam/sdks/go/pkg/beam"
+	"github.com/apache/beam/sdks/go/pkg/beam/io/textio"
+	"github.com/apache/beam/sdks/go/pkg/beam/log"
+	"github.com/apache/beam/sdks/go/pkg/beam/transforms/stats"
+	"github.com/apache/beam/sdks/go/pkg/beam/x/beamx"
+
+	// The following packages are required to read files from GCS or local.
+	_ "github.com/apache/beam/sdks/go/pkg/beam/io/filesystem/gcs"
+	_ "github.com/apache/beam/sdks/go/pkg/beam/io/filesystem/local"
+)
+
+func init() {
+	beam.RegisterType(reflect.TypeOf((*parseLineFn)(nil)).Elem())
+	beam.RegisterFunction(formatResultFn)
+}
+
+var (
+	inputURI  = flag.String("input_uri", "", "Input file with a number on each line.")
+	outputURI = flag.String("output_uri", "", "Output file.")
+)
+
+func main() {
+	flag.Parse()
+	beam.Init()
+
+	ctx := context.Background()
+	pipeline := beam.NewPipeline()
+
+	scope := pipeline.Root()
+	lines := textio.Read(scope, *inputURI)
+	reshuffledLines := beam.Reshuffle(scope, lines)
+
+	values := beam.ParDo(scope, &parseLineFn{}, reshuffledLines)
+	sum := stats.Sum(scope, values)
+
+	formatted := beam.ParDo(scope, formatResultFn, sum)
+	textio.Write(scope, *outputURI, formatted)
+
+	if err := beamx.Run(ctx, pipeline); err != nil {
+		log.Exitf(ctx, "Failed to execute job: %s", err)
+	}
+}
+
+type parseLineFn struct {
+	lineCounter beam.Counter
+}
+
+func (fn *parseLineFn) Setup() {
+	fn.lineCounter = beam.NewCounter("simple_example", "parse-line-count")
+}
+
+func (fn *parseLineFn) ProcessElement(ctx context.Context, line string, emit func(uint64)) error {
+	value, err := strconv.ParseUint(line, 10, 64)
+	if err != nil {
+		return err
+	}
+	emit(value)
+	fn.lineCounter.Inc(ctx, 1)
+	return nil
+}
+
+func formatResultFn(sum uint64, emit func(string)) error {
+	emit(strconv.FormatUint(sum, 10))
+	return nil
+}
